15292fbce0b28749e17fa512dbac024fc9692cf2
Brian Finn <brian.finn@spire.com>
2025-04-29 16:18:06 -0600

    test copying of attrs objects and try out uv and ruff

--- python-attrs/attrs-example.py
+++ python-attrs/attrs-example.py
@@ -1,13 +1,33 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "attrs==19.3.0",
+#     "rich",
+# ]
+# ///
+import copy
 import attr
+from rich.pretty import pprint
+
 
 @attr.s
 class Foo(object):
     a = attr.ib()
     b = attr.ib()
 
+
 def main():
-    f = Foo(1,2)
-    print(f)
+    f = Foo(1, 2)
+    pprint(f)
+    pprint(dir(f))
+
+    f2 = copy.deepcopy(f)
+    pprint(f2)
+    pprint(dir(f2))
+
+    print(f"same obj: {id(f) == id(f2)}")
+    print(f"same cmp: {f == f2}")
+
 
 if __name__ == "__main__":
     main()
